124b96f1f1edc5989df912a2770a87c117038028
shashank singh <singh.shashank166948@gmail.com>
2020-12-17 07:34:58 -0500

    input-output-processing

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 var btntranslate=document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 console.log(txtInput);
+var outputDiv= document.querySelector("#output")
+
 
 btntranslate.addEventListener("click",function clickEventHandler(){
-    console.log("clicked"); 
-    console.log("input",txtInput.value);
-})
+   outputDiv.innerHTML="kuhsfsjgzs : "+ txtInput.value;
+});
 
 
        
